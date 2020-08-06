d9038941105219637827bc853f0822f702d8f5d4
mistyjack <oladepomustafa@gmail.com>
2020-08-06 23:07:16 +0100

    Created package.json and webpack config file.

---
+++ webpack.config.js
@@ -0,0 +1,85 @@
+const currentTask = process.env.npm_lifecycle_event
+const path = require("path")
+const Dotenv = require("dotenv-webpack")
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const HtmlWebpackHarddiskPlugin = require("html-webpack-harddisk-plugin")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const fse = require("fs-extra")
+
+const postCSSPlugins = [require("postcss-import"), require("postcss-mixins"), require("postcss-simple-vars"), require("postcss-nested"), require("postcss-hexrgba"), require("autoprefixer")]
+
+class RunAfterCompile {
+  apply(compiler) {
+    compiler.hooks.done.tap("Copy images", function () {
+      fse.copySync("./app/assets/images", "./docs/assets/images")
+    })
+  }
+}
+
+let cssConfig = {
+  test: /\.css$/i,
+  use: ["css-loader?url=false", { loader: "postcss-loader", options: { plugins: postCSSPlugins } }]
+}
+
+let config = {
+  entry: "./app/assets/scripts/App.js",
+  plugins: [
+    new Dotenv(),
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "app/index-template.html",
+      alwaysWriteToDisk: true
+    }),
+    new HtmlWebpackHarddiskPlugin()
+  ],
+  module: {
+    rules: [cssConfig]
+  }
+}
+
+if (currentTask == "webpackDev" || currentTask == "dev") {
+  cssConfig.use.unshift("style-loader")
+  config.output = {
+    filename: "bundled.js",
+    path: path.resolve(__dirname, "app")
+  }
+  config.devServer = {
+    before: function (app, server) {
+      server._watch("./app/**/*.html")
+    },
+    contentBase: path.join(__dirname, "app"),
+    hot: true,
+    port: 3000,
+    host: "0.0.0.0"
+  }
+  config.mode = "development"
+}
+
+if (currentTask == "webpackBuild") {
+  config.module.rules.push({
+    test: /\.js$/,
+    exclude: /(node_modules)/,
+    use: {
+      loader: "babel-loader",
+      options: {
+        presets: ["@babel/preset-react", ["@babel/preset-env", { targets: { node: "12" } }]]
+      }
+    }
+  })
+
+  cssConfig.use.unshift(MiniCssExtractPlugin.loader)
+  postCSSPlugins.push(require("cssnano"))
+  config.output = {
+    filename: "[name].[chunkhash].js",
+    chunkFilename: "[name].[chunkhash].js",
+    path: path.resolve(__dirname, "docs")
+  }
+  config.mode = "production"
+  config.optimization = {
+    splitChunks: { chunks: "all" }
+  }
+  config.plugins.push(new CleanWebpackPlugin(), new MiniCssExtractPlugin({ filename: "styles.[chunkhash].css" }), new RunAfterCompile())
+}
+
+module.exports = config
